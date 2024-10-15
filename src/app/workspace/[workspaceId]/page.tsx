52cc0d13e23abdaa2ff9b90044082d66d3a647d6
Jaco-Miras <miras.jaco@gmail.com>
2024-10-16 02:57:34 +0800

    fix on workspaces

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspaces";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 
 const WorkspaceIdPage = () => {
   const workspaceId = useWorkspaceId();
