655361cc53885a3a2102c48e0bde034442ec515a
Jaco-Miras <miras.jaco@gmail.com>
2024-11-13 01:57:18 +0800

    editor component

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { Header } from "./header";
+import { ChatInput } from "./chat-input";
+
 import { Loader, TriangleAlert } from "lucide-react";
 
 import { useGetChannel } from "@/features/channels/api/use-get-channel";
 import { useChannelId } from "@/hooks/use-channel-id";
-import { Header } from "./header";
 
 const ChannelIdPage = () => {
   const channelId = useChannelId();
@@ -33,6 +35,9 @@ const ChannelIdPage = () => {
   return (
     <div className="flex flex-col h-full">
       <Header title={channel.name} />
+      <div className="flex-1" />
+
+      <ChatInput />
     </div>
   );
 };
